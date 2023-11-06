1e4429e66267f1b5af796703512461c08f4c4194
aayushkap <aayushkap321@gmail.com>
2023-11-06 12:17:11 +0400

    EDIT Fix

--- app/edit/[id]/page.jsx
+++ app/edit/[id]/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import EditMovieForm from "@/components/EditMovieForm";
 
 async function getMovieById(id) {
@@ -5,7 +7,6 @@ async function getMovieById(id) {
     const res = await fetch(`/api/movies/${id}`, {
       cache: "no-store",
     });
-
     return res.json();
   } catch (error) {
     console.log(error);
@@ -17,12 +18,19 @@ export default async function Edit({ params }) {
 
   const data = await getMovieById(id);
 
+  console.log(data);
+
   const { movie } = data;
   const { title, genre, watched } = movie;
 
+  console.log(title, genre, watched);
+
   return (
-    <div>
-      <EditMovieForm id={id} title={title} genre={genre} watched={watched} />
-    </div>
+    <>
+      <div>
+        <EditMovieForm id={id} title={title} genre={genre} watched={watched} />
+      </div>
+      ;
+    </>
   );
 }
